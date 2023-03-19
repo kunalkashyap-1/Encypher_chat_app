50262aa3caecfbdffe2a6f706368d2f31f5ff28f
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2023-03-19 23:08:58 +0530

    something is cooking

---
+++ client/src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function useFetch(imgData) {
+//   const [imgLink, setImgLink] = useState(null);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     if(!imgData){
+//       setIsLoading(false);
+//       return;
+//     }
+
+//     const fetchData = () => {
+      
+//     };
+//     fetchData();
+//   }, [imgData]);
+
+//   return { imgLink, isLoading, error };
+// }
+
+// export default useFetch;
